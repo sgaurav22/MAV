3da82939916fa4441aaf69301e0ee60095c56dcf
Gaurav <gsaxena@cgsinc.in>
2013-04-10 13:41:57 +0530

    Module MAV-1
    1. Registration Part is done

--- grails-app/domain/com/mav/module/student/StudentRegistration.groovy
+++ grails-app/domain/com/mav/module/student/StudentRegistration.groovy
@@ -4,4 +4,32 @@ class StudentRegistration {
 
     static constraints = {
     }
+    
+    String studentId
+    String firstName
+    String middleName
+    String fatherName
+    String motherName
+    int age
+    Date dob
+    String currentHouseNumber
+    String currentStreet
+    String currentCity    
+    String currentState
+    String country
+    int currentHomePhoneNumber
+    int currentMobilePhoneNumber
+    String permanentHouseNumber
+    String permanentStreet
+    String permanentCity    
+    String permanentState
+    String country
+    int permanentHomePhoneNumber
+    int permanentMobilePhoneNumber
+    String fatherOccupation
+    String motherOccupation
+    String previousSchoolName
+    String previousClass
+    String previousSchoolAddress
+        
 }
