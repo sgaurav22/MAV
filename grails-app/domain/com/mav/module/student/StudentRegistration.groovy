41bd30ab68871357a33b92aaf513551ce4ea7fb2
Gaurav <gsaxena@cgsinc.in>
2013-04-10 13:10:53 +0530

    Project MAV

---
+++ grails-app/domain/com/mav/module/student/StudentRegistration.groovy
@@ -0,0 +1,7 @@
+package com.mav.module.student
+
+class StudentRegistration {
+
+    static constraints = {
+    }
+}
---
+++ test/unit/com/mav/module/student/StudentRegistrationTests.groovy
@@ -0,0 +1,17 @@
+package com.mav.module.student
+
+import grails.test.*
+
+class StudentRegistrationTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
